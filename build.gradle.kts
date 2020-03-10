04ba46e81cb72945016fe2a44b370a2f82f15fea
Matheus Gambati <matheusgambati@gmail.com>
2020-03-09 20:22:06 -0400

    Configura gradlew para gerar JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,29 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    kotlin("kapt") version "1.3.70"
 }
 
 group = "dev.mgambati"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("de.vandermeer:asciitable:0.3.2")
+    compile(kotlin("stdlib-jdk8"))
+    compile("de.vandermeer:asciitable:0.3.2")
+    compile("info.picocli:picocli:4.2.0")
+    kapt("info.picocli:picocli-codegen:4.2.0")
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    archiveBaseName.set("compiler")
+    manifest {
+        attributes["Main-Class"] = "dev.mgambati.CompilerKt"
+    }
+    from(configurations.runtime.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks["jar"] as CopySpec)
 }
 
 tasks {
@@ -20,5 +32,15 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    build {
+        dependsOn(fatJar)
+    }
+}
+
+kapt {
+    arguments {
+        arg("project", "${project.group}/${project.name}")
     }
 }
